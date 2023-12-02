2ec41aa2432a07021bd30d2a0bf0e8a718e2c884
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2023-12-02 14:36:03 +0100

    day 1

---
+++ 2022/day1/src/bin/part1.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part_1(input);
+    dbg!(output);
+}
+
+fn part_1(input: &str) -> String {
+    dbg!(input);
+    "".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let input = include_str!("./input1_ex.txt");
+        let r = part_1(input);
+        assert_eq!("24000", r);
+    }
+}
---
+++ 2022/day1/src/bin/part2.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let input = include_str!("./input2.txt");
+    let output = part_2(input);
+    dbg!(output);
+}
+
+fn part_2(input: &str) -> String {
+    "".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let input = include_str!("./input2_ex.txt");
+        let r = part_2(input);
+        assert_eq!("", r);
+    }
+}
