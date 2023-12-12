7bdd1ab59be36daa1beeb775a61e54e3ffcc8ca6
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2023-12-12 01:24:50 +0100

    day 9 part 1

--- 2023/day9/src/bin/part1.rs
+++ 2023/day9/src/bin/part1.rs
@@ -1,3 +1,5 @@
+mod common;
+
 fn main() {
     let input = include_str!("./input1.txt");
     let output = part_1(input);
@@ -5,7 +7,18 @@ fn main() {
 }
 
 fn part_1(input: &str) -> String {
-    "".to_string()
+    let mut r = 0;
+    for line in input.lines() {
+        let numbers = common::get_init_numbers(line);
+        let mut compute = vec![numbers];
+        while !compute.last().unwrap().iter().all(|n| n == &0) {
+            let differences = common::get_differences(&mut compute);
+            compute.push(differences);
+        }
+
+        r += compute.iter().map(|row| row.last().unwrap()).sum::<isize>();
+    }
+    r.to_string()
 }
 
 #[cfg(test)]
