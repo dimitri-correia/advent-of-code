a2127d3d55e05931c767f86b3b25b6494b67b38c
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2023-12-15 19:21:25 +0100

    day 11 part 2 wip

--- 2023/day11/src/bin/part1.rs
+++ 2023/day11/src/bin/part1.rs
@@ -1,3 +1,5 @@
+mod common;
+
 fn main() {
     let input = include_str!("./input1.txt");
     let output = part_1(input);
@@ -5,74 +7,11 @@ fn main() {
 }
 
 fn part_1(input: &str) -> String {
-    let distances = calculate_distances(input);
+    let distances = common::calculate_distances(input, 1);
 
     let sum: usize = distances.iter().map(|d| d.dist).sum();
 
     sum.to_string()
-}
-
-#[derive(Debug)]
-struct Dist {
-    from: Pos,
-    to: Pos,
-    dist: usize,
-}
-
-#[derive(Debug)]
-struct Pos {
-    x: usize,
-    y: usize,
-}
-
-fn calculate_distances(input: &str) -> Vec<Dist> {
-    let points = get_points(input);
-
-    let mut distances = vec![];
-    for (i, &(x, y)) in points.iter().enumerate() {
-        for &(xx, yy) in points.iter().skip(i + 1) {
-            let dist =
-                ((xx as isize - x as isize).abs() + (yy as isize - y as isize).abs()) as usize;
-            let from = Pos { x, y };
-            let to = Pos { x: xx, y: yy };
-            distances.push(Dist { from, to, dist })
-        }
-    }
-
-    distances
-}
-
-fn get_points(input: &str) -> Vec<(usize, usize)> {
-    let mut shift = 0;
-    let points: Vec<(usize, usize)> = input
-        .lines()
-        .enumerate()
-        .flat_map(|(x, line)| {
-            if !line.contains('#') {
-                shift += 1;
-            }
-            line.chars()
-                .enumerate()
-                .filter_map(move |(y, c)| if c == '#' { Some((x + shift, y)) } else { None })
-        })
-        .collect();
-
-    let mut shift = vec![];
-    for i in 0..input.lines().next().unwrap().len() {
-        if input
-            .lines()
-            .all(|line| line.chars().nth(i).unwrap() == '.')
-        {
-            shift.push(i);
-        }
-    }
-
-    let points: Vec<(usize, usize)> = points
-        .iter()
-        .map(|(a, b)| (*a, *b + shift.iter().filter(|&&i| i < *b).count()))
-        .collect();
-
-    points
 }
 
 #[cfg(test)]
--- 2023/day11/src/bin/part2.rs
+++ 2023/day11/src/bin/part2.rs
@@ -1,11 +1,16 @@
+mod common;
 fn main() {
     let input = include_str!("./input1.txt"); //same
-    let output = part_2(input);
+    let output = part_2(input, 1_000_000);
     dbg!(output);
 }
 
-fn part_2(input: &str) -> String {
-    "".to_string()
+fn part_2(input: &str, i: usize) -> String {
+    let distances = common::calculate_distances(input, i);
+
+    let sum: usize = distances.iter().map(|d| d.dist).sum();
+
+    sum.to_string()
 }
 
 #[cfg(test)]
@@ -13,9 +18,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn it_works_10() {
         let input = include_str!("./input1_ex.txt"); // same
-        let r = part_2(input);
-        assert_eq!("", r);
+        let r = part_2(input, 10);
+        assert_eq!("1030", r);
+    }
+
+    #[test]
+    fn it_works_100() {
+        let input = include_str!("./input1_ex.txt"); // same
+        let r = part_2(input, 100);
+        assert_eq!("8410", r);
     }
 }
