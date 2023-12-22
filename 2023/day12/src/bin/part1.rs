4d0bb277b67ee7656172a871663e1afc2912f8cf
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2023-12-22 20:17:06 +0100

    day 5 part 1

--- 2023/day12/src/bin/part1.rs
+++ 2023/day12/src/bin/part1.rs
@@ -4,7 +4,7 @@ fn main() {
     dbg!(output);
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 enum SpringState {
     OK,
     KO,
@@ -27,10 +27,7 @@ fn parse_str_to_spring_state(c: char) -> SpringState {
 }
 
 fn part_1(input: &str) -> String {
-    dbg!(&input);
-
     let map = parse_input(input);
-    dbg!(&map);
 
     let mut r = 0;
     for line in map {
@@ -41,7 +38,64 @@ fn part_1(input: &str) -> String {
 }
 
 fn get_nb_arrangements(line: Line) -> usize {
-    0
+    let mut r = 0;
+    for possibility in generate_lines_possible(&line) {
+        if count_hashes_in_row(possibility) == line.order {
+            r += 1;
+        }
+    }
+    r
+}
+
+fn generate_lines_possible(line: &Line) -> Vec<Vec<SpringState>> {
+    let mut result = Vec::new();
+    generate_strings_helper(&line.line_state, 0, Vec::new(), &mut result);
+    result
+}
+
+fn generate_strings_helper(
+    line: &Vec<SpringState>,
+    index: usize,
+    mut current: Vec<SpringState>,
+    result: &mut Vec<Vec<SpringState>>,
+) {
+    if index == line.len() {
+        result.push(current);
+    } else {
+        let current_state = *line.get(index).unwrap();
+
+        if current_state == SpringState::Unknown {
+            let mut new_current = current.clone();
+            new_current.push(SpringState::OK);
+            generate_strings_helper(line, index + 1, new_current, result);
+
+            current.push(SpringState::KO);
+            generate_strings_helper(line, index + 1, current, result);
+        } else {
+            current.push(current_state);
+            generate_strings_helper(line, index + 1, current, result);
+        }
+    }
+}
+
+fn count_hashes_in_row(line: Vec<SpringState>) -> Vec<usize> {
+    let mut counts = Vec::new();
+    let mut current_count = 0;
+
+    for state in line {
+        if state == SpringState::KO {
+            current_count += 1;
+        } else if current_count > 0 {
+            counts.push(current_count);
+            current_count = 0;
+        }
+    }
+
+    if current_count > 0 {
+        counts.push(current_count);
+    }
+
+    counts
 }
 
 fn parse_input(input: &str) -> Vec<Line> {
