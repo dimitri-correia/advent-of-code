7d300ba2319eb2c223e08fa9f9c7ff26445a8c0b
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-01-28 14:52:49 +0100

    part 1 ok

--- src/y2023/day09/common.rs
+++ src/y2023/day09/common.rs
@@ -1,17 +1,24 @@
-pub fn get_differences(compute: &mut [Vec<isize>]) -> Vec<isize> {
-    let differences: Vec<isize> = compute
+pub fn get_compute(line: &str) -> Vec<Vec<isize>> {
+    let numbers = get_init_numbers(line);
+    let mut compute = vec![numbers];
+    while !compute.last().unwrap().iter().all(|n| n == &0) {
+        let differences = get_differences(&mut compute);
+        compute.push(differences);
+    }
+    compute
+}
+
+fn get_differences(compute: &mut [Vec<isize>]) -> Vec<isize> {
+    compute
         .last()
         .unwrap()
         .windows(2)
         .map(|pair| pair[1] - pair[0])
-        .collect();
-    differences
+        .collect()
 }
 
-pub fn get_init_numbers(line: &str) -> Vec<isize> {
-    let numbers = line
-        .split_whitespace()
+fn get_init_numbers(line: &str) -> Vec<isize> {
+    line.split_whitespace()
         .map(|c| c.parse::<isize>().unwrap())
-        .collect::<Vec<isize>>();
-    numbers
+        .collect::<Vec<isize>>()
 }
--- src/y2023/day09/part1.rs
+++ src/y2023/day09/part1.rs
@@ -1,18 +1,14 @@
-use crate::y2023::day09::common;
+use crate::y2023::day09::common::get_compute;
 
 fn part_1(input: &str) -> String {
-    let mut r = 0;
-    for line in input.lines() {
-        let numbers = common::get_init_numbers(line);
-        let mut compute = vec![numbers];
-        while !compute.last().unwrap().iter().all(|n| n == &0) {
-            let differences = common::get_differences(&mut compute);
-            compute.push(differences);
-        }
-
-        r += compute.iter().map(|row| row.last().unwrap()).sum::<isize>();
-    }
-    r.to_string()
+    input
+        .lines()
+        .map(|line| {
+            let compute = get_compute(line);
+            compute.iter().map(|row| row.last().unwrap()).sum::<isize>()
+        })
+        .sum::<isize>()
+        .to_string()
 }
 
 #[cfg(test)]
