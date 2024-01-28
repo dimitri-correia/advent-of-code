daf2a9f0a9365a130e59d1619c84e7e5e784288f
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-01-28 14:52:57 +0100

    part 2 ok

--- src/y2023/day09/part2.rs
+++ src/y2023/day09/part2.rs
@@ -1,21 +1,17 @@
-use crate::y2023::day09::common;
+use crate::y2023::day09::common::get_compute;
 
 fn part_2(input: &str) -> String {
-    let mut r = 0;
-    for line in input.lines() {
-        let numbers = common::get_init_numbers(line);
-        let mut compute = vec![numbers];
-        while !compute.last().unwrap().iter().all(|n| n == &0) {
-            let differences = common::get_differences(&mut compute);
-            compute.push(differences);
-        }
-
-        r += compute
-            .iter()
-            .rev()
-            .fold(0, |acc, l| l.first().map_or(acc, |&first| first - acc));
-    }
-    r.to_string()
+    input
+        .lines()
+        .map(|line| {
+            let compute = get_compute(line);
+            compute
+                .iter()
+                .rev()
+                .fold(0, |acc, l| l.first().map_or(acc, |&first| first - acc))
+        })
+        .sum::<isize>()
+        .to_string()
 }
 
 #[cfg(test)]
