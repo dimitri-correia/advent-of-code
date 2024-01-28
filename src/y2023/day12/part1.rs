600f25cbfb036380aa63b8d26b45c5a18c4df470
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-01-28 23:41:45 +0100

    part 1 ok

--- src/y2023/day12/part1.rs
+++ src/y2023/day12/part1.rs
@@ -21,24 +21,18 @@ fn parse_str_to_spring_state(c: char) -> SpringState {
 }
 
 fn part_1(input: &str) -> String {
-    let map = parse_input(input);
-
-    let mut r = 0;
-    for line in map {
-        r += get_nb_arrangements(line);
-    }
-
-    r.to_string()
+    parse_input(input)
+        .iter()
+        .map(|line| get_nb_arrangements(line))
+        .sum::<usize>()
+        .to_string()
 }
 
-fn get_nb_arrangements(line: Line) -> usize {
-    let mut r = 0;
-    for possibility in generate_lines_possible(&line) {
-        if count_hashes_in_row(possibility) == line.order {
-            r += 1;
-        }
-    }
-    r
+fn get_nb_arrangements(line: &Line) -> usize {
+    generate_lines_possible(line)
+        .iter()
+        .filter(|possibility| check_validity(possibility, line.order.clone()))
+        .count()
 }
 
 fn generate_lines_possible(line: &Line) -> Vec<Vec<SpringState>> {
@@ -48,7 +42,7 @@ fn generate_lines_possible(line: &Line) -> Vec<Vec<SpringState>> {
 }
 
 fn generate_strings_helper(
-    line: &Vec<SpringState>,
+    line: &[SpringState],
     index: usize,
     mut current: Vec<SpringState>,
     result: &mut Vec<Vec<SpringState>>,
@@ -56,12 +50,19 @@ fn generate_strings_helper(
     if index == line.len() {
         result.push(current);
     } else {
-        let current_state = *line.get(index).unwrap();
+        let current_state = line[index];
 
         if current_state == SpringState::Unknown {
-            let mut new_current = current.clone();
-            new_current.push(SpringState::OK);
-            generate_strings_helper(line, index + 1, new_current, result);
+            generate_strings_helper(
+                line,
+                index + 1,
+                {
+                    let mut new_current = current.clone();
+                    new_current.push(SpringState::OK);
+                    new_current
+                },
+                result,
+            );
 
             current.push(SpringState::KO);
             generate_strings_helper(line, index + 1, current, result);
@@ -72,12 +73,12 @@ fn generate_strings_helper(
     }
 }
 
-fn count_hashes_in_row(line: Vec<SpringState>) -> Vec<usize> {
+fn check_validity(line: &&Vec<SpringState>, expected: Vec<usize>) -> bool {
     let mut counts = Vec::new();
     let mut current_count = 0;
 
-    for state in line {
-        if state == SpringState::KO {
+    for state in line.iter() {
+        if state == &SpringState::KO {
             current_count += 1;
         } else if current_count > 0 {
             counts.push(current_count);
@@ -89,26 +90,25 @@ fn count_hashes_in_row(line: Vec<SpringState>) -> Vec<usize> {
         counts.push(current_count);
     }
 
-    counts
+    counts == expected
 }
 
 fn parse_input(input: &str) -> Vec<Line> {
-    let mut map = Vec::with_capacity(input.lines().count());
-    for line in input.lines() {
-        let mut line_state = Vec::with_capacity(line.len());
-        let parts: Vec<&str> = line.split_whitespace().collect();
-        for c in parts[0].chars() {
-            let state = parse_str_to_spring_state(c);
-            line_state.push(state);
-        }
-        let order: Vec<usize> = parts[1]
-            .split(',')
-            .map(|s| s.parse::<usize>().unwrap())
-            .collect();
-        map.push(Line { line_state, order })
-    }
-
-    map
+    input
+        .lines()
+        .map(|line| {
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            let line_state: Vec<SpringState> = parts[0]
+                .chars()
+                .map(|c| parse_str_to_spring_state(c))
+                .collect();
+            let order: Vec<usize> = parts[1]
+                .split(',')
+                .map(|s| s.parse::<usize>().unwrap())
+                .collect();
+            Line { line_state, order }
+        })
+        .collect()
 }
 
 #[cfg(test)]
