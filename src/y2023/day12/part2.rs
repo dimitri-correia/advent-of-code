f71db8e642cb95cc0c40d6a5b2dad7889f9d5287
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-02-03 21:35:14 +0100

    quick fail for day 12 part 2

--- src/y2023/day12/part2.rs
+++ src/y2023/day12/part2.rs
@@ -46,6 +46,7 @@ mod tests {
 
     #[test]
     fn actual_challenge() {
+        panic!();
         let input = include_str!("input1.txt");
         let output = part_2(input);
         dbg!(&output);
@@ -54,6 +55,7 @@ mod tests {
 
     #[test]
     fn example_test() {
+        panic!();
         let input = include_str!("input1_ex.txt"); // same file
         let r = part_2(input);
         assert_eq!("525152", r);
