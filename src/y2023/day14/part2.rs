e37a24866b728ab518fed4d6d6146a5f7c8c25c3
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-02-04 21:33:48 +0100

    wip part 2

--- src/y2023/day14/part2.rs
+++ src/y2023/day14/part2.rs
@@ -1,6 +1,29 @@
+use crate::y2023::day14::common::{get_val_col, parse_input, Shape};
+
 fn part_2(input: &str) -> String {
     const NUMBER_CYCLE: usize = 1_000_000_000;
-    "".to_string()
+    let grid = parse_input(input);
+    let final_grid = (0..NUMBER_CYCLE)
+        .into_iter()
+        .fold(grid, |tmp_grid, _| do_one_cycle(tmp_grid));
+
+    final_grid
+        .into_iter()
+        .map(get_val_col)
+        .sum::<usize>()
+        .to_string()
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum TiltDir {
+    North,
+    West,
+    South,
+    East,
+}
+
+fn do_one_cycle(grid: Vec<Vec<Shape>>) -> Vec<Vec<Shape>> {
+    grid
 }
 
 #[cfg(test)]
