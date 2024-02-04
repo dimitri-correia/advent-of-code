e076303547e617dd98d06cb7d8ab1393d6ef6361
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-02-04 20:16:38 +0100

    day 13 part 1 ok

--- src/y2023/day13/part1.rs
+++ src/y2023/day13/part1.rs
@@ -1,3 +1,6 @@
+use itertools::Itertools;
+use std::convert::AsRef;
+
 fn part_1(input: &str) -> String {
     input
         .split("\n\n")
@@ -8,18 +11,39 @@ fn part_1(input: &str) -> String {
 }
 
 fn get_res_one_pattern(input: &str) -> usize {
-    match try_find_horizontal_fold(&input) {
+    match try_find_horizontal_fold(input) {
         Some(res) => 100 * res,
-        _ => try_find_vertical_fold(&input),
+        _ => try_find_vertical_fold(&input).unwrap(),
     }
 }
 
-fn try_find_vertical_fold(input: &&str) -> usize {
-    todo!()
+fn try_find_vertical_fold(lines_input: &str) -> Option<usize> {
+    let lines: Vec<&str> = lines_input.lines().collect();
+
+    let col: Vec<String> = (0..lines[0].len())
+        .map(|i| lines.iter().map(|s| s[i..=i].to_owned()).collect())
+        .collect();
+
+    find_fold(col.iter().map(|c| c.as_str()).collect())
 }
 
-fn try_find_horizontal_fold(input: &&str) -> Option<usize> {
-    todo!()
+fn try_find_horizontal_fold(lines_input: &str) -> Option<usize> {
+    find_fold(lines_input.lines().collect())
+}
+
+fn find_fold(lines: Vec<&str>) -> Option<usize> {
+    lines
+        .iter()
+        .enumerate()
+        .tuple_windows()
+        .filter(|((_, line_a), (_, line_b))| line_a == line_b)
+        .find_map(|(_, (index_b, _))| {
+            let (before, after) = lines.split_at(index_b);
+
+            let all_lines_equal = before.iter().rev().zip(after).all(|(a, b)| a == b);
+
+            all_lines_equal.then_some(index_b)
+        })
 }
 
 #[cfg(test)]
@@ -31,7 +55,7 @@ mod tests {
         let input = include_str!("input1.txt");
         let output = part_1(input);
         dbg!(&output);
-        assert_eq!("val", output);
+        assert_eq!("29213", output);
     }
 
     #[test]
