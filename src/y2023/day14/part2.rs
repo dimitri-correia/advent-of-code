5516441401152b0e1e1d13f014d5cf74d8e36b6b
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-02-10 00:28:41 +0100

    3 cycle OK

--- src/y2023/day14/part2.rs
+++ src/y2023/day14/part2.rs
@@ -1,58 +1,79 @@
-use crate::y2023::day14::common::{change_oder_col_row, get_val_col, parse_input_row_col, Shape};
+use crate::y2023::day14::common::{
+    change_oder_col_row, get_val_col, parse_input_col_row, Grid, Order, Shape,
+};
 
 fn part_2(input: &str) -> String {
     const NUMBER_CYCLE: usize = 1_000_000_000;
 
     // as first element of cycle is north, we need to have the first grid in row_col format
-    let grid = parse_input_row_col(input);
+    let grid = parse_input_col_row(input);
 
     // also in row_col format
     let final_grid = get_final_grid(grid, NUMBER_CYCLE);
 
     final_grid
+        .grid
         .into_iter()
         .map(get_val_col) // todo
         .sum::<usize>()
         .to_string()
 }
 
-///
-/// take a grid in the row_col format and cycle over it `number_cycle` times to return it in the same format
-///
-fn get_final_grid(grid: Vec<Vec<Shape>>, number_cycle: usize) -> Vec<Vec<Shape>> {
+fn get_final_grid(grid: Grid, number_cycle: usize) -> Grid {
+    assert_eq!(grid.order, Order::ColRow);
     let final_grid = (0..number_cycle).into_iter().fold(grid, |tmp_grid, _| {
-        // maybe optimize and stop if same grid as before
         do_east(do_south(do_west(do_north(tmp_grid))))
     });
+    assert_eq!(final_grid.order, Order::ColRow);
     final_grid
 }
 
-fn do_east(grid: Vec<Vec<Shape>>) -> Vec<Vec<Shape>> {
-    change_oder_col_row(grid)
-        .iter()
-        .map(|line| move_o_right(line))
-        .collect()
+fn do_east(grid: Grid) -> Grid {
+    assert_eq!(grid.order, Order::RowCol);
+    let new_grid = change_oder_col_row(grid);
+
+    Grid {
+        grid: new_grid
+            .grid
+            .iter()
+            .map(|line| move_o_right(line))
+            .collect(),
+        order: new_grid.order,
+    }
 }
 
-fn do_south(grid: Vec<Vec<Shape>>) -> Vec<Vec<Shape>> {
-    change_oder_col_row(grid)
-        .iter()
-        .map(|line| move_o_right(line))
-        .collect()
+fn do_south(grid: Grid) -> Grid {
+    assert_eq!(grid.order, Order::ColRow);
+    let new_grid = change_oder_col_row(grid);
+
+    Grid {
+        grid: new_grid
+            .grid
+            .iter()
+            .map(|line| move_o_right(line))
+            .collect(),
+        order: new_grid.order,
+    }
 }
 
-fn do_west(grid: Vec<Vec<Shape>>) -> Vec<Vec<Shape>> {
-    change_oder_col_row(grid)
-        .iter()
-        .map(|line| move_o_left(line))
-        .collect()
+fn do_west(grid: Grid) -> Grid {
+    assert_eq!(grid.order, Order::RowCol);
+    let new_grid = change_oder_col_row(grid);
+
+    Grid {
+        grid: new_grid.grid.iter().map(|line| move_o_left(line)).collect(),
+        order: new_grid.order,
+    }
 }
 
-fn do_north(grid: Vec<Vec<Shape>>) -> Vec<Vec<Shape>> {
-    change_oder_col_row(grid)
-        .iter()
-        .map(|line| move_o_left(line))
-        .collect()
+fn do_north(grid: Grid) -> Grid {
+    assert_eq!(grid.order, Order::ColRow);
+    let new_grid = change_oder_col_row(grid);
+
+    Grid {
+        grid: new_grid.grid.iter().map(|line| move_o_left(line)).collect(),
+        order: new_grid.order,
+    }
 }
 
 fn move_o_right(v: &Vec<Shape>) -> Vec<Shape> {
@@ -119,7 +140,7 @@ fn move_o_left(v: &Vec<Shape>) -> Vec<Shape> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::y2023::day14::common::parse_char;
+    use crate::y2023::day14::common::{parse_char, parse_input_row_col};
     use itertools::Itertools;
 
     // #[test]
@@ -130,17 +151,17 @@ mod tests {
     //     assert_eq!("val", output);
     // }
     //
-    // #[test]
-    // fn example_test() {
-    //     let input = include_str!("input1_ex.txt"); // same file
-    //     let r = part_2(input);
-    //     assert_eq!("64", r);
-    // }
+    #[test]
+    fn example_test() {
+        let input = include_str!("input1_ex.txt"); // same file
+        let r = part_2(input);
+        assert_eq!("64", r);
+    }
 
     #[test]
     fn test_do_north() {
         let input = include_str!("input1_ex.txt");
-        let before = parse_input_row_col(input);
+        let before = parse_input_col_row(input);
         let expected = parse_input_row_col(
             "OOOO.#.O..
 OO..#....#
@@ -194,7 +215,7 @@ O..#.OO...
     #[test]
     fn test_3_cycles() {
         let input = include_str!("input1_ex.txt"); // same file
-        let grid = parse_input_row_col(input);
+        let grid = parse_input_col_row(input);
         let expected = [
             ".....#....
 ....#...O#
@@ -224,31 +245,39 @@ O..#.OO...
 .O#...O#.#
 ....O#...O
 .......OOO
-#...O###.O",
+#...O###.O
+#.OOO#...O",
         ];
         let mut new_grid = grid.clone();
         for ex in expected {
             new_grid = get_final_grid(new_grid, 1);
-            let res = parse_input_row_col(ex);
-            assert_eq!(res, new_grid);
+            let res = parse_input_col_row(ex);
+            helper_print(&res);
+            assert_eq!(res.order, new_grid.order);
+            assert_eq!(res.grid, new_grid.grid);
             dbg!("one cycle ok");
         }
     }
 
-    fn helper_print(v: &Vec<Vec<Shape>>) -> String {
-        let p = change_oder_col_row(v.clone())
+    fn helper_print(v: &Grid) -> String {
+        fn inverse_parse(s: &Shape) -> char {
+            match s {
+                Shape::RoundedRock => 'O',
+                Shape::Empty => '.',
+                Shape::CubeRock => '#',
+            }
+        }
+        let g = if v.order == Order::RowCol {
+            change_oder_col_row(v.clone())
+        } else {
+            v.clone()
+        };
+        let p = g
+            .grid
             .iter()
             .map(|row| row.iter().map(|s| inverse_parse(s)).collect::<String>())
             .join("\n");
 
         dbg!(p)
-    }
-
-    pub fn inverse_parse(s: &Shape) -> char {
-        match s {
-            Shape::RoundedRock => 'O',
-            Shape::Empty => '.',
-            Shape::CubeRock => '#',
-        }
     }
 }
