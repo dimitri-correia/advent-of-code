1e38a60a47ecbd4d51330162b04d6648bc465b41
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-02-12 20:56:09 +0100

    part 1 OK with common

--- src/y2023/day16/common.rs
+++ src/y2023/day16/common.rs
@@ -1,3 +1,50 @@
+use std::collections::HashSet;
+
+pub fn get_number_energized_tiles(grid: &Vec<Vec<Tile>>, dir: Dir, pos: Coord) -> usize {
+    let mut energized: HashSet<Position> = HashSet::new();
+    get_dirs_from_tile(&Position { pos, dir }, grid[0][0])
+        .iter()
+        .for_each(|&dir| {
+            let pos = Position { pos, dir };
+            follow_path(&grid, &mut energized, pos);
+        });
+    energized
+        .iter()
+        .map(|p| p.pos)
+        .collect::<HashSet<Coord>>()
+        .len()
+}
+
+fn follow_path(grid: &[Vec<Tile>], energized: &mut HashSet<Position>, pos: Position) {
+    if energized.contains(&pos) {
+        return;
+    }
+    energized.insert(pos);
+    get_next(&pos, &grid)
+        .iter()
+        .for_each(|&next_pos| follow_path(grid, energized, next_pos));
+}
+
+fn get_next(curr_pos: &Position, grid: &[Vec<Tile>]) -> Vec<Position> {
+    let x = curr_pos.pos.0 + curr_pos.dir.coordinates().0;
+    let y = curr_pos.pos.1 + curr_pos.dir.coordinates().1;
+    if x < 0 || x as usize >= grid.len() || y < 0 || y as usize >= grid[0].len() {
+        return vec![];
+    }
+    let new_cord = Coord(x, y);
+    let tile = grid[x as usize][y as usize];
+
+    let new_dir = get_dirs_from_tile(curr_pos, tile);
+
+    new_dir
+        .iter()
+        .map(|dir| Position {
+            pos: new_cord,
+            dir: *dir,
+        })
+        .collect()
+}
+
 pub fn get_dirs_from_tile(curr_pos: &Position, tile: Tile) -> Vec<Dir> {
     let new_dir = match tile {
         Tile::Empty => vec![curr_pos.dir],
--- src/y2023/day16/part1.rs
+++ src/y2023/day16/part1.rs
@@ -1,63 +1,8 @@
-use crate::y2023::day16::common::{get_dirs_from_tile, parse_input, Coord, Dir, Position, Tile};
-use std::collections::HashSet;
-use std::vec;
+use crate::y2023::day16::common::{get_number_energized_tiles, parse_input, Coord, Dir};
 
 fn part_1(input: &str) -> String {
     let grid = parse_input(input);
-
-    let mut energized: HashSet<Position> = HashSet::new();
-
-    let coord = Coord(0, 0);
-
-    get_dirs_from_tile(
-        &Position {
-            pos: coord,
-            dir: Dir::Right,
-        },
-        grid[0][0],
-    )
-    .iter()
-    .for_each(|&dir| {
-        let pos = Position { pos: coord, dir };
-        follow_path(&grid, &mut energized, pos);
-    });
-
-    energized
-        .iter()
-        .map(|p| p.pos)
-        .collect::<HashSet<Coord>>()
-        .len()
-        .to_string()
-}
-
-fn follow_path(grid: &[Vec<Tile>], energized: &mut HashSet<Position>, pos: Position) {
-    if energized.contains(&pos) {
-        return;
-    }
-    energized.insert(pos);
-    get_next(&pos, &grid)
-        .iter()
-        .for_each(|&next_pos| follow_path(grid, energized, next_pos));
-}
-
-fn get_next(curr_pos: &Position, grid: &[Vec<Tile>]) -> Vec<Position> {
-    let x = curr_pos.pos.0 + curr_pos.dir.coordinates().0;
-    let y = curr_pos.pos.1 + curr_pos.dir.coordinates().1;
-    if x < 0 || x as usize >= grid.len() || y < 0 || y as usize >= grid[0].len() {
-        return vec![];
-    }
-    let new_cord = Coord(x, y);
-    let tile = grid[x as usize][y as usize];
-
-    let new_dir = get_dirs_from_tile(curr_pos, tile);
-
-    new_dir
-        .iter()
-        .map(|dir| Position {
-            pos: new_cord,
-            dir: *dir,
-        })
-        .collect()
+    get_number_energized_tiles(&grid, Dir::Right, Coord(0, 0)).to_string()
 }
 
 #[cfg(test)]
