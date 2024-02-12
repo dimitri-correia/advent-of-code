d0156398cd9aa4e556d1d8ce36355d89a3ff74bb
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-02-12 04:22:00 +0100

    remove all useless comments

--- src/y2022/day01/part2.rs
+++ src/y2022/day01/part2.rs
@@ -13,7 +13,7 @@ mod tests {
 
     #[test]
     fn actual_challenge() {
-        let input = include_str!("input1.txt"); //same
+        let input = include_str!("input1.txt");
         let output = part_2(input);
         dbg!(&output);
         assert_eq!("210367", output);
