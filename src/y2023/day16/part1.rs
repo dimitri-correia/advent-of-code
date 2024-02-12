a176a801cffb1a799ac5fc7c0929a34647bc5e4e
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-02-12 17:44:37 +0100

    part 1 ok

--- src/y2023/day16/part1.rs
+++ src/y2023/day16/part1.rs
@@ -150,7 +150,7 @@ mod tests {
         let input = include_str!("input1.txt");
         let output = part_1(input);
         dbg!(&output);
-        assert_eq!("val", output);
+        assert_eq!("7798", output);
     }
 
     #[test]
