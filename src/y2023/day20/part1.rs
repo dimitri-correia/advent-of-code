3478d64625fe370bbe8242ccfafd72d2fe165cbf
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-02-27 00:46:25 +0100

    parsing ok

--- src/y2023/day20/part1.rs
+++ src/y2023/day20/part1.rs
@@ -1,5 +1,52 @@
+use std::collections::HashMap;
+
 fn part_1(input: &str) -> String {
+    let modules = parse_input(input);
+    dbg!(&modules);
     "".to_string()
+}
+
+fn parse_input(input: &str) -> HashMap<String, Module> {
+    input
+        .lines()
+        .map(|l| {
+            let mut parts = l.trim().split("->");
+            let source_dest = parts.next().unwrap().trim();
+            let module_type = match source_dest.as_bytes()[0] {
+                b'&' => Type::Conjunction,
+                b'%' => Type::FlipFLop,
+                _ => Type::Broadcaster,
+            };
+            let name = (&source_dest[1..]).to_string();
+            let destinations: Vec<String> = parts
+                .next()
+                .unwrap()
+                .split(',')
+                .map(|s| s.trim().to_string())
+                .collect();
+
+            (
+                name,
+                Module {
+                    module_type,
+                    destinations,
+                },
+            )
+        })
+        .collect()
+}
+
+#[derive(Debug)]
+enum Type {
+    FlipFLop,
+    Conjunction,
+    Broadcaster,
+}
+
+#[derive(Debug)]
+struct Module {
+    module_type: Type,
+    destinations: Vec<String>,
 }
 
 #[cfg(test)]
