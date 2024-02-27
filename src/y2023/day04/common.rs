c049320e8ea0d34491ecd9cb3791cd40514e4ced
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-02-27 14:21:20 +0100

    rustfmt

--- src/y2023/day04/common.rs
+++ src/y2023/day04/common.rs
@@ -1,14 +1,14 @@
 pub fn get_card_infos(line: &str) -> (usize, Vec<u32>, Vec<u32>) {
     let mut parts = line.split(':');
-    
+
     let card_id = parts
         .next()
         .and_then(|s| s.trim().split_whitespace().nth(1))
         .and_then(|s| s.parse::<usize>().ok())
         .unwrap_or_default();
-    
+
     let card_info = parts.next().unwrap_or_default();
-    
+
     let (winning_numbers, elf_numbers) = extract_numbers(card_info);
 
     (card_id, winning_numbers, elf_numbers)
