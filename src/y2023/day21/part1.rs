446a196da382e21187821f4af4b915f3a0ec0e86
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-05 12:06:09 +0100

    day 21 ok

--- src/y2023/day21/part1.rs
+++ src/y2023/day21/part1.rs
@@ -1,22 +1,63 @@
-use std::collections::HashSet;
-
-fn part_1(input: &str, steps_remaining: i32) -> String {
+fn part_1(input: &str, steps_remaining: i32, print_grid: bool) -> String {
     let (start_position, grid) = parse_input(input);
-    dbg!(&start_position, &grid);
-    let mut actual_pos = HashSet::new();
-    actual_pos.insert(start_position);
 
-    dbg!(&actual_pos);
+    let mut actual_pos = vec![];
+    actual_pos.push(start_position);
 
     (0..steps_remaining).for_each(|_| {
-        dbg!("a");
         let size = actual_pos.len();
         (0..size).for_each(|_| {
-            actual_pos.remove(0);
+            let pos = actual_pos.remove(0);
+            get_next(pos, &grid, &mut actual_pos);
         })
     });
 
-    "".to_string()
+    if print_grid {
+        print_final_grid(&grid, &actual_pos);
+    }
+
+    actual_pos.len().to_string()
+}
+
+fn print_final_grid(grid: &Vec<Vec<Tile>>, possible_pos: &Vec<Position>) {
+    for (x, row) in grid.iter().enumerate() {
+        for (y, tile) in row.iter().enumerate() {
+            let pos = Position {
+                x: x as isize,
+                y: y as isize,
+            };
+            if possible_pos.contains(&pos) {
+                print!("O");
+            } else {
+                match tile {
+                    Tile::Rock => print!("#"),
+                    Tile::GardenPlot => print!("."),
+                    _ => panic!(),
+                }
+            }
+        }
+        println!();
+    }
+}
+
+fn get_next(pos: Position, grid: &Vec<Vec<Tile>>, actual_pos: &mut Vec<Position>) {
+    [(0, 1), (0, -1), (1, 0), (-1, 0)].iter().for_each(|mov| {
+        let potential_pos = pos.move_dir(mov);
+        if is_pos_outside(potential_pos, &grid) || !is_walkable_position(grid, potential_pos) {
+            return;
+        }
+        if !actual_pos.contains(&potential_pos) {
+            actual_pos.push(potential_pos);
+        }
+    });
+}
+
+fn is_walkable_position(grid: &Vec<Vec<Tile>>, potential_pos: Position) -> bool {
+    Tile::GardenPlot == grid[potential_pos.x as usize][potential_pos.y as usize]
+}
+
+fn is_pos_outside(pos: Position, grid: &&Vec<Vec<Tile>>) -> bool {
+    pos.x < 0 || pos.x >= grid.len() as isize || pos.y < 0 || pos.y >= grid[0].len() as isize
 }
 
 fn parse_input(input: &str) -> (Position, Vec<Vec<Tile>>) {
@@ -31,8 +72,11 @@ fn parse_input(input: &str) -> (Position, Vec<Vec<Tile>>) {
                     '#' => Tile::Rock,
                     '.' => Tile::GardenPlot,
                     'S' => {
-                        start = Some(Position { x, y });
-                        Tile::Rock
+                        start = Some(Position {
+                            x: x as isize,
+                            y: y as isize,
+                        });
+                        Tile::GardenPlot
                     }
                     _ => panic!(),
                 })
@@ -42,13 +86,22 @@ fn parse_input(input: &str) -> (Position, Vec<Vec<Tile>>) {
     (start.unwrap(), grid)
 }
 
-#[derive(Debug, Eq, PartialEq, Hash)]
+#[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
 struct Position {
-    x: usize,
-    y: usize,
+    x: isize,
+    y: isize,
 }
 
-#[derive(Debug)]
+impl Position {
+    fn move_dir(&self, mov: &(isize, isize)) -> Self {
+        Position {
+            x: self.x + mov.0,
+            y: self.y + mov.1,
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
 enum Tile {
     Rock,
     Start,
@@ -62,15 +115,15 @@ mod tests {
     #[test]
     fn actual_challenge() {
         let input = include_str!("input1.txt");
-        let output = part_1(input, 64);
+        let output = part_1(input, 64, false);
         dbg!(&output);
-        assert_eq!("", output);
+        assert_eq!("3574", output);
     }
 
     #[test]
     fn example_test() {
         let input = include_str!("input1_ex.txt");
-        let r = part_1(input, 6);
+        let r = part_1(input, 6, false);
         assert_eq!("16", r);
     }
 }
