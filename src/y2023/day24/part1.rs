ae64bcecc0c1c9565a69212d98132da4ff0e7083
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-06 10:28:11 +0100

    day 24 init

---
+++ src/y2023/day24/part1.rs
@@ -0,0 +1,23 @@
+fn part_1(input: &str) -> String {
+    "".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn actual_challenge() {
+        let input = include_str!("input1.txt");
+        let output = part_1(input);
+        dbg!(&output);
+        assert_eq!("", output);
+    }
+
+    #[test]
+    fn example_test() {
+        let input = include_str!("input1_ex.txt");
+        let r = part_1(input);
+        assert_eq!("", r);
+    }
+}
