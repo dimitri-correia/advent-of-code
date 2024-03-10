555b5fdeb612e22fea2b20fcca9cf3f7ec040c3e
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-10 22:58:21 +0100

    day 3 part 2 ok

--- src/y2022/day03/part2.rs
+++ src/y2022/day03/part2.rs
@@ -1,5 +1,25 @@
+use itertools::Itertools;
+
 fn part_2(input: &str) -> String {
-    "".to_string()
+    let mut lines = input.lines();
+    let mut r = 0;
+    while let Some(line_a) = lines.next() {
+        let line_b = lines.next().unwrap();
+        let line_c = lines.next().unwrap();
+        let c = line_a
+            .chars()
+            .find(|c| line_b.chars().contains(c) && line_c.chars().contains(c))
+            .unwrap();
+        let common = (if c.is_ascii_lowercase() {
+            c as u8 - b'a' + 1
+        } else {
+            // uppercase
+            c as u8 - b'A' + 27
+        }) as u32;
+        r += common;
+    }
+
+    r.to_string()
 }
 
 #[cfg(test)]
@@ -11,12 +31,12 @@ mod tests {
         let input = include_str!("input1.txt");
         let output = part_2(input);
         dbg!(&output);
-        assert_eq!("", output);
+        assert_eq!("2363", output);
     }
     #[test]
     fn example_test() {
         let input = include_str!("input1_ex.txt");
         let r = part_2(input);
-        assert_eq!("", r);
+        assert_eq!("70", r);
     }
 }
