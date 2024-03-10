5b2d8accaa1b622411e03d71ddee6fd0015f5d00
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-10 22:15:41 +0100

    day 3 part 1 ok

--- src/y2022/day03/part1.rs
+++ src/y2022/day03/part1.rs
@@ -1,5 +1,23 @@
 fn part_1(input: &str) -> String {
-    "".to_string()
+    input
+        .lines()
+        .fold(0, |acc, line| {
+            let line_bytes = line.as_bytes();
+            let size = line_bytes.len();
+            let idx_common = (0..size / 2)
+                .find(|&i| line_bytes[size / 2..].contains(&line_bytes[i]))
+                .unwrap();
+
+            let common = (if line_bytes[idx_common].is_ascii_lowercase() {
+                line_bytes[idx_common] - b'a' + 1
+            } else {
+                // uppercase
+                line_bytes[idx_common] - b'A' + 27
+            }) as u32;
+
+            acc + common
+        })
+        .to_string()
 }
 
 #[cfg(test)]
@@ -11,13 +29,13 @@ mod tests {
         let input = include_str!("input1.txt");
         let output = part_1(input);
         dbg!(&output);
-        assert_eq!("", output);
+        assert_eq!("8105", output);
     }
 
     #[test]
     fn example_test() {
         let input = include_str!("input1_ex.txt");
         let r = part_1(input);
-        assert_eq!("", r);
+        assert_eq!("157", r);
     }
 }
