642318344db137b8ebcd0cdb796ec6f5ff74ef61
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-10 23:29:39 +0100

    part 1 ok

--- src/y2022/day04/part1.rs
+++ src/y2022/day04/part1.rs
@@ -1,5 +1,23 @@
 fn part_1(input: &str) -> String {
-    "".to_string()
+    input
+        .lines()
+        .filter(|l| {
+            let (elf_a, elf_b) = l.split_once(',').unwrap();
+            let elf_a = get_elf_range(elf_a);
+            let elf_b = get_elf_range(elf_b);
+            contains(elf_a, elf_b)
+        })
+        .count()
+        .to_string()
+}
+
+fn contains(a: (usize, usize), b: (usize, usize)) -> bool {
+    b.0 >= a.0 && b.1 <= a.1 || a.0 >= b.0 && a.1 <= b.1
+}
+
+fn get_elf_range(elf_a: &str) -> (usize, usize) {
+    let (elf_start, elf_end) = elf_a.split_once('-').unwrap();
+    (elf_start.parse().unwrap(), elf_end.parse().unwrap())
 }
 
 #[cfg(test)]
@@ -11,13 +29,13 @@ mod tests {
         let input = include_str!("input1.txt");
         let output = part_1(input);
         dbg!(&output);
-        assert_eq!("", output);
+        assert_eq!("424", output);
     }
 
     #[test]
     fn example_test() {
         let input = include_str!("input1_ex.txt");
         let r = part_1(input);
-        assert_eq!("", r);
+        assert_eq!("2", r);
     }
 }
