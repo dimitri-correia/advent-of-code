b08c01746201bb42d0901abf48af3d76395c5156
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-12 00:12:37 +0100

    parsing ok

--- src/y2023/day12/common.rs
+++ src/y2023/day12/common.rs
@@ -11,7 +11,25 @@ pub struct Line {
     pub order: Vec<usize>,
 }
 
-pub fn parse_str_to_spring_state(c: char) -> SpringState {
+pub fn parse_input(input: &str) -> Vec<Line> {
+    input
+        .lines()
+        .map(|line| {
+            let (line_state, order) = line.split_once(' ').unwrap();
+            let line_state: Vec<SpringState> = line_state
+                .chars()
+                .map(|c| parse_str_to_spring_state(c))
+                .collect();
+            let order: Vec<usize> = order
+                .split(',')
+                .map(|s| s.parse::<usize>().unwrap())
+                .collect();
+            Line { line_state, order }
+        })
+        .collect()
+}
+
+fn parse_str_to_spring_state(c: char) -> SpringState {
     match c {
         '#' => SpringState::KO,
         '.' => SpringState::OK,
